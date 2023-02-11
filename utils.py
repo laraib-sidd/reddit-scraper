91625a7e72ff92975b34f695c3a5f46889fb26fa
laraibsidd <laraib.siddiqui@lybrate.com>
2023-02-11 23:59:19 +0530

    Add all the functions to utils

--- utils.py
+++ utils.py
@@ -99,30 +99,12 @@ def load_csv_posts_pmaw(date_range, args):
         posts = api.search_submissions(
             subreddit=sub_name, limit=3, before=ed, after=sd)
         print(
-            f'Retrieved {len(posts)} comments from Pushshift for {month} and {ddate}')
+            f'Retrieved {len(posts)} Posts from Pushshift for {month} and {ddate}')
         li = []
-        for i in posts:
-            posts_dict = dict()
-            posts_dict['subreddit'] = i['subreddit']
-            posts_dict['selftext'] = "".join(i['selftext'].splitlines())
-            posts_dict['author_fullname'] = i['author_fullname']
-            posts_dict['title'] = i['title']
-            posts_dict['total_awards_received'] = i['total_awards_received']
-            posts_dict['score'] = i['score']
-            posts_dict['removed_by_category'] = i['removed_by_category']
-            posts_dict['over_18'] = i['over_18']
-            posts_dict['media_only'] = i['media_only']
-            posts_dict['locked'] = i['locked']
-            posts_dict['subreddit_id'] = i['subreddit_id']
-            posts_dict['post_id'] = i['id']
-            posts_dict['author'] = i['author']
-            posts_dict['num_comments'] = i['num_comments']
-            posts_dict['post_url'] = f"https://www.reddit.com/{i['permalink']}"
-            posts_dict['num_crossposts'] = i['num_crossposts']
-            posts_dict['post_date'] = i['utc_datetime_str']
-            posts_dict['created_utc'] = i['created_utc']
-            # print(d)
-            li.append(posts_dict)
+        for post in posts:
+            post['selftext'] = "".join(post['selftext'].splitlines())
+            post['post_url'] = f"https://www.reddit.com{post['permalink']}"
+            li.append(post)
 
         df = pd.DataFrame.from_dict(li, orient='columns')
         df.to_csv(
@@ -147,35 +129,16 @@ def load_mongo_posts_pmaw(date_range, client, args):
         posts = api.search_submissions(
             subreddit=sub_name, limit=3, before=ed, after=sd)
         print(
-            f'Retrieved {len(posts)} comments from Pushshift for {month} and {ddate}')
+            f'Retrieved {len(posts)} posts from Pushshift for {month} and {ddate}')
         li = []
-        for i in posts:
-            posts_dict = dict()
-            posts_dict['subreddit'] = i['subreddit']
-            posts_dict['selftext'] = "".join(i['selftext'].splitlines())
-            posts_dict['author_fullname'] = i['author_fullname']
-            posts_dict['title'] = i['title']
-            posts_dict['total_awards_received'] = i['total_awards_received']
-            posts_dict['score'] = i['score']
-            posts_dict['removed_by_category'] = i['removed_by_category']
-            posts_dict['over_18'] = i['over_18']
-            posts_dict['media_only'] = i['media_only']
-            posts_dict['locked'] = i['locked']
-            posts_dict['subreddit_id'] = i['subreddit_id']
-            posts_dict['post_id'] = i['id']
-            posts_dict['author'] = i['author']
-            posts_dict['num_comments'] = i['num_comments']
-            posts_dict['post_url'] = f"https://www.reddit.com/{i['permalink']}"
-            posts_dict['num_crossposts'] = i['num_crossposts']
-            posts_dict['post_date'] = i['utc_datetime_str']
-            posts_dict['created_utc'] = i['created_utc']
-            # print(d)
-            li.append(posts_dict)
+        for post in posts:
+            post['selftext'] = "".join(post['selftext'].splitlines())
+            post['post_url'] = f"https://www.reddit.com{post['permalink']}"
+            li.append(post)
         mycol.insert_many(li)
 
 
 def load_mongo_posts_api(date_range, client, args):
-    api = PushshiftAPI()
     sub_name = args.sub_name
 
     mydb = client["test"]
@@ -189,36 +152,117 @@ def load_mongo_posts_api(date_range, client, args):
         sd = int(dt.strptime(date_range[i], '%Y-%m-%d').timestamp())
         ed = int(dt.strptime(date_range[i+1], '%Y-%m-%d').timestamp())
 
-        url = f"https://api.pushshift.io/reddit/search/submission/?subreddit=bitcoin&metadata=true&limit=3&since={sd}&until={ed}"
+        url = f"https://api.pushshift.io/reddit/search/submission/?subreddit={sub_name}&metadata=true&limit=3&since={sd}&until={ed}"
         response = urllib.request.urlopen(url)
         text = response.read()
         req_data = json.loads(text.decode('utf-8'))
 
         posts = req_data['data']
         print(
-            f'Retrieved {len(posts)} comments from Pushshift for {month} and {ddate}')
+            f'Retrieved {len(posts)} posts from Pushshift for {month} and {ddate}')
         li = []
-        for i in posts:
-            posts_dict = dict()
-            posts_dict['subreddit'] = i['subreddit']
-            posts_dict['selftext'] = "".join(i['selftext'].splitlines())
-            posts_dict['author_fullname'] = i['author_fullname']
-            posts_dict['title'] = i['title']
-            posts_dict['total_awards_received'] = i['total_awards_received']
-            posts_dict['score'] = i['score']
-            posts_dict['removed_by_category'] = i['removed_by_category']
-            posts_dict['over_18'] = i['over_18']
-            posts_dict['media_only'] = i['media_only']
-            posts_dict['locked'] = i['locked']
-            posts_dict['subreddit_id'] = i['subreddit_id']
-            posts_dict['post_id'] = i['id']
-            posts_dict['author'] = i['author']
-            posts_dict['num_comments'] = i['num_comments']
-            posts_dict['post_url'] = f"https://www.reddit.com/{i['permalink']}"
-            posts_dict['num_crossposts'] = i['num_crossposts']
-            posts_dict['post_date'] = i['utc_datetime_str']
-            posts_dict['created_utc'] = i['created_utc']
-            # print(d)
-            li.append(posts_dict)
+        for post in posts:
+            post['selftext'] = "".join(post['selftext'].splitlines())
+            post['post_url'] = f"https://www.reddit.com{post['permalink']}"
+            li.append(post)
+            load_comments_mongo(post['id'],ddate,month,client)
         mycol.insert_many(li)
 
+
+def load_csv_posts_api(date_range, args):
+    sub_name = args.sub_name
+    for i in range(0, len(date_range)-1):
+        s = dt.strptime(date_range[i], '%Y-%m-%d')
+        ddate = s.strftime('%Y-%m-%d')
+        month = s.strftime('%B')
+
+        sub_path = f"test/data/posts/{sub_name}"
+        if not os.path.exists(sub_path):
+            os.makedirs(sub_path)
+
+        date_path = f"test/data/posts/{sub_name}/{month}"
+        if not os.path.exists(date_path):
+            os.makedirs(date_path)
+
+        sd = int(dt.strptime(date_range[i], '%Y-%m-%d').timestamp())
+        ed = int(dt.strptime(date_range[i+1], '%Y-%m-%d').timestamp())
+
+        url = f"https://api.pushshift.io/reddit/search/submission/?subreddit={sub_name}&metadata=true&limit=3&since={sd}&until={ed}"
+        response = urllib.request.urlopen(url)
+        text = response.read()
+        req_data = json.loads(text.decode('utf-8'))
+
+        posts = req_data['data']
+        print(
+            f'Retrieved {len(posts)} posts from Pushshift for {month} and {ddate}')
+        li = []
+        for post in posts:
+            post['selftext'] = "".join(post['selftext'].splitlines())
+            post['post_url'] = f"https://www.reddit.com{post['permalink']}"
+            load_comments_csv(post["id"],sub_name,ddate,month)
+            li.append(post)
+
+        df = pd.DataFrame.from_dict(li, orient='columns')
+        df.to_csv(
+            f"test/data/posts/{sub_name}/{month}/{ddate}_posts.csv", index=False)
+
+
+def load_comments_csv(pid, sub_name, ddate, month):
+
+    sub_path = f"test/data/comments/{sub_name}"
+    if not os.path.exists(sub_path):
+        os.makedirs(sub_path)
+
+    date_path = f"test/data/comments/{sub_name}/{month}"
+    if not os.path.exists(date_path):
+        os.makedirs(date_path)
+    p_id_path = f"test/data/comments/{sub_name}/{month}/{ddate}/{pid}"
+    if not os.path.exists(p_id_path):
+        os.makedirs(p_id_path)
+        
+    base_10_id = int(pid,36)
+    url = f"https://api.pushshift.io/reddit/search/comment?link_id={base_10_id}&limit=3"
+    response = urllib.request.urlopen(url)
+    text = response.read()
+    req_data = json.loads(text.decode('utf-8'))
+
+    comments = req_data['data']
+    print(
+        f'Retrieved {len(comments)} comments from Pushshift for {month} and {ddate} and{pid}')
+    if len(comments) == 0:
+        return
+    li = []
+    for comment in comments: 
+        comment["post_id"] = pid     
+        comment['body'] = "".join(comment['body'].splitlines())
+        comment['comment_url'] = f"https://www.reddit.com{comment['permalink']}"
+        li.append(comment)
+
+    df = pd.DataFrame.from_dict(li, orient='columns')
+    df.to_csv(
+        f"test/data/comments/{sub_name}/{month}/{ddate}/{pid}/comments.csv", index=False)
+
+
+def load_comments_mongo(pid, ddate, month,client):
+    mydb = client["test"]
+    mycol = mydb["comments"]
+        
+    base_10_id = int(pid,36)
+    url = f"https://api.pushshift.io/reddit/search/comment?link_id={base_10_id}&limit=1000"
+    response = urllib.request.urlopen(url)
+    text = response.read()
+    req_data = json.loads(text.decode('utf-8'))
+
+    comments = req_data['data']
+    print(
+        f'Retrieved {len(comments)} comments from Pushshift for {month} and {ddate} and{pid}')
+    if len(comments) == 0:
+        return
+    li = []
+    for comment in comments: 
+        comment["post_id"] = pid     
+        comment['body'] = "".join(comment['body'].splitlines())
+        comment['comment_url'] = f"https://www.reddit.com{comment['permalink']}"
+        li.append(comment)
+
+    mycol.insert_many(li)
